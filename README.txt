bb485d41554fc61ed610daee2fe264e8740fecc8
Hanno Schlichting <hanno@jarn.com>
2009-05-22 22:16:55 +0000

    Even more updates

---
+++ README.txt
@@ -0,0 +1,9 @@
+Overview
+========
+
+This implementation of Gettext for Python includes a Msgfmt class which can be
+used to generate compiled mo files from Gettext po files and includes support
+for the newer msgctxt keyword. The idea for this project had been rather
+ambitious, but never lived up to what is was supposed to do. Look at Babel
+(http://pypi.python.org/pypi/Babel) for a package more worthy of this packages'
+name.
--- setup.py
+++ setup.py
@@ -5,14 +5,8 @@ version = '1.0'
 setup(name='python-gettext',
       version=version,
       description="Python Gettext implementation.",
-      long_description="""\
-This implementation of Gettext for Python includes a Msgfmt class which can be
-used to generate compiled mo files from Gettext po files and includes support
-for the newer msgctxt keyword. The idea for this project had been rather
-ambitious, but never lived up to what is was supposed to do. Look at Babel
-(http://pypi.python.org/pypi/Babel) for a package more worthy of this
-packages' name.
-""",
+      long_description=open("README.txt").read() +  "\n" +
+                       open("CHANGES.txt").read(),
       classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -26,7 +20,7 @@ packages' name.
       author_email='hannosch@hannosch.eu',
       url='http://pypi.python.org/pypi/python-gettext',
       license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'tests']),
+      packages=find_packages(exclude=['ez_setup']),
       include_package_data=True,
       zip_safe=False,
       )
