3a3bde497d0d7ba18fc4af1bb832900db3642403
Hanno Schlichting <hanno@jarn.com>
2011-12-22 18:50:37 +0100

    Refine unicode header tests - add one failing test on Python 2.x

--- pythongettext/tests/test_compile.py
+++ pythongettext/tests/test_compile.py
@@ -85,13 +85,16 @@ class TestWriter(unittest.TestCase):
             po_file.close()
         self.assertEqual(po.encoding, u('utf-8'))
 
+    def TODO_test_test6(self):
+        self.compare_po_mo('test6.po', 'test6.mo')
+
     def test_test6_unicode_header(self):
         po_file = open(os.path.join(FOLDER, 'test6.po'), 'rb')
         po = Msgfmt(po_file)
         po.read(header_only=True)
         po_file.close()
         self.assertTrue(
-            po.messages[u('')].startswith(u('Project-Id-Version: Töst')))
+            po.messages[u('')].startswith(u('Project-Id-Version: Tøst 1.0')))
         self.assertEqual(po.encoding, u('utf-8'))
 
     def test_escape(self):
